07d232ac5e1bfd86b043e9d16d1c1e77ef260551
Jackson Kitsao <kitsaojackson22@gmail.com>
2020-07-17 15:21:17 +0300

    finished styling the page

---
+++ app.js
@@ -0,0 +1,10 @@
+//caching the DOM elements
+const year = document.getElementById("year");
+const month = document.getElementById("month");
+const gender = document.getElementById("gender");
+const button = document.getElementById("submit");
+//variable initilization
+var cc = null;
+var yy = null;
+var mm = null;
+var dd = null;
