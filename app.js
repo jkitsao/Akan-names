4dd84796f4a4a8c3274d189e279edf9334084a38
Jackson Kitsao <kitsaojackson22@gmail.com>
2020-07-17 16:07:33 +0300

    add javascript to the page

--- app.js
+++ app.js
@@ -1,10 +1,36 @@
 //caching the DOM elements
-const year = document.getElementById("year");
-const month = document.getElementById("month");
-const gender = document.getElementById("gender");
-const button = document.getElementById("submit");
+// const year = document.getElementById("year");
+// const month = document.getElementById("month");
+// const gender = document.getElementById("gender");
+// const button = document.getElementById("submit");
 //variable initilization
-var cc = null;
-var yy = null;
+
 var mm = null;
 var dd = null;
+//function to get the cc/centuary details
+//the function gets the two first digits of a number
+const cc = (y) => {
+  let convert = y.toString();
+  let sec = convert.split("")[0] + convert.split("")[1];
+  let num = Number(sec);
+  return num;
+};
+// console.log(cc(1978));
+
+//get last year digits
+//we
+const yy = (y) => {
+  let convert = y.toString();
+  let sec = convert.split("")[2] + convert.split("")[3];
+  let num = Number(sec);
+  return num;
+};
+// console.log(yy(1978));
+
+//function to get the day of the month
+const getDay = (cc, yy, mm, dd) => {
+  return Math.floor(
+    (cc / 4 - 2 * cc - 1 + (5 * yy) / 4 + (26 * (mm + 1)) / 10 + dd) % 7
+  );
+};
+console.log(getDay(cc(1943), yy(1943), 2, 17));
